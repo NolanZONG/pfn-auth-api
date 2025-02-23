a205c3bb8bba1857facd77300e641a55375b27cb
Nan ZONG <zongnan1989@gmail.com>
2025-02-23 19:06:42 +0900

    auth done

---
+++ auth_api/auth.py
@@ -0,0 +1,31 @@
+"""
+HTTP Basic Auth module
+
+This module is used to process http basic auth globally.
+"""
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+
+from auth_api.repository import AuthDataRepository
+
+security = HTTPBasic()
+
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    """
+    HTTP basic auth
+    """
+    repo = AuthDataRepository()
+
+    fetched_auth_data = repo.fetch_user(credentials.username)
+    if not fetched_auth_data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="auth_user_not_found")
+
+    if not secrets.compare_digest(credentials.password, fetched_auth_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="auth_failed"
+        )
+    return credentials.username
--- auth_api/main.py
+++ auth_api/main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, status, Depends
 from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
 
+from auth_api.auth import authenticate
 from auth_api.database import Base, engine, SessionLocal
 from auth_api.model import AuthData
 from auth_api.repository import AuthDataRepository
@@ -21,7 +22,6 @@ async def lifespan(app: FastAPI):
         print(e)
     finally:
         session.close()
-
     yield
 
 
@@ -46,10 +46,12 @@ async def signup(request_body: SignupRequest):
         }
     }
 
+
 @app.get("/users/{user_id}")
-async def get_user(user_id: str):
+async def get_user(user_id: str, auth_user_id: str = Depends(authenticate)):
     c = repo_service.fetch_user("zong")
-    return {"message": f"{c.password}"}
+    return {"user_id": f"{user_id}", "name": auth_user_id}
+
 
 
 @app.patch("/users/{user_id}")
@@ -66,7 +68,7 @@ async def delete_user():
 
 @app.exception_handler(HTTPException)
 async def custom_http_exception_handler(request: Request, exc: HTTPException):
-    if exc.status_code == 400 and exc.detail == "insert_user_duplicated":
+    if exc.status_code == status.HTTP_400_BAD_REQUEST and exc.detail == "insert_user_duplicated":
         return JSONResponse(
             status_code=exc.status_code,
             content={
@@ -74,32 +76,45 @@ async def custom_http_exception_handler(request: Request, exc: HTTPException):
                 "cause": "already same user_id is used"
             }
         )
+    if exc.status_code == status.HTTP_404_NOT_FOUND:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"message": "No User found"}
+        )
 
+    if exc.status_code == status.HTTP_401_UNAUTHORIZED:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"message": "Authentication Failed"},
+            headers={"WWW-Authenticate": "Basic"}
+        )
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     for error in exc.errors():
         if error["type"] == "missing":
             return JSONResponse(
-                status_code=400,
+                status_code=status.HTTP_400_BAD_REQUEST,
                 content={
                     "message": "Account creation failed",
                     "cause": "required user_id and password"
                 }
             )
         return JSONResponse(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             content={
                 "message": "Account creation failed",
                 "cause": f"{'.'.join(str(loc) for loc in error["loc"][1:])}:{error['msg']}"
             }
         )
 
+
 @app.exception_handler(ValueError)
 async def value_error_handler(request: Request, exc: ValueError):
     return JSONResponse(
-        status_code=400,
+        status_code=status.HTTP_400_BAD_REQUEST,
         content={
             "message": "Account creation failed",
             "cause": str(exc)
