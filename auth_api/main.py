c03fc652914c0011c1eacd567fea3017d191ed4e
Nan ZONG <zongnan1989@gmail.com>
2025-02-24 09:55:02 +0900

    fix patch bug

--- auth_api/main.py
+++ auth_api/main.py
@@ -73,7 +73,7 @@ async def get_user(user_id: str, auth_user_id: str = Depends(authenticate)):
 async def patch_user(user_id: str, request_body: UpdateAccountRequest, auth_user_id: str = Depends(authenticate)):
     if user_id != auth_user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No permission for Update")
-    if "user_id" in UpdateAccountRequest or "password" in UpdateAccountRequest:
+    if "user_id" in request_body.model_fields_set or "password" in request_body.model_fields_set:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={
